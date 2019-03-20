56a362440c6b29de19f25b1b236c0a43ad968b4c
Wykthor <wykthor.g@wgvale.com>
2019-03-20 13:24:08 -0300

    O jeito fácil é o jeito fácil

--- 01_Basics.py
+++ 01_Basics.py
@@ -1,7 +1,7 @@
 import pygame
-from functools import reduce
 from pygame import Vector2 as Vec2
-from copy import deepcopy
+from sys import argv
+import math
 pygame.init()
 
 display_width = 800
@@ -32,55 +32,43 @@ class Car(Particle):
     def __init__(self,x,y, display,path, vectors=list()):
         super().__init__(x,y)
         self.img = pygame.image.load(path)
-
         self.vectors = vectors
         self.vector = Vec2()
         self.display = display
-        self.show()
 
     def __str__(self):
         return "pos ({},{}) vec ({},{})".format(self.x,self.y,self.vector.x,self.vector.y)
 
-    def force(self, forceVector):
-        self.vectors.append(forceVector)
-
-    # def calcMove(self):
-
-    def move(self):
-        if self.vectors:
-            self.vector = deepcopy(reduce(lambda x,y:x+y, self.vectors))
-        if self.vector.length():
-            self.vector /= self.vector.length()
-        self.x, self.y = self+self.vector
+    def newPos(self,radius,theta,center):
+        self.x = center.x+radius*math.cos(theta)
+        self.y = center.y+radius*math.sin(theta)
         self.show()
 
     def show(self):
         self.display.blit(self.img,(self.x,self.y))
-        # pygame.draw.line(self.display, black, (self.x, self.y),((self.x+self.vector.x),(self.y+self.vector.y)),5)
 
-def game_loop():
+def game_loop(radius = 50):
     x = (display_width * 0.5)
     y = (display_height * 0.5)
     gameExit = False
     center = Car(x,y,gameDisplay,"racecar.png",[])
     gameDisplay.fill(white)
-    center.move()
-    Orbital = Vec2(2500,0) # Substituir por radius**2
-    car = Car(x,y-50,gameDisplay,"racecar.png",[Orbital])
-    toCenter = car.aim(center)
+    center.show()
+    radius = int(radius)
+    car = Car(x,y-radius,gameDisplay,"racecar.png",[])
+    theta = 0
     while not gameExit:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
-        toCenter.x, toCenter.y = car.aim(center).x, car.aim(center).y
-        Orbital.x, Orbital.y = Orbital+toCenter
-        car.move()
+        theta+=0.01
+        car.newPos(radius,theta,center)
         pygame.display.update()
         clock.tick(60)
 
 
-game_loop()
+game_loop(argv[1])
 pygame.quit()
 quit()
